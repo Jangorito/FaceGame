b48a6ef3096d5ec426faef0d8112d7fa2e8c4ad5
Jack Brannan <psyjb25@exmail.nottingham.ac.uk>
2023-12-04 12:27:56 +0000

    Addition: add facemesh tesselation

--- UnityPythonMediaPipeAvatar-main/mediapipeavatar/body.py
+++ UnityPythonMediaPipeAvatar-main/mediapipeavatar/body.py
@@ -93,6 +93,12 @@ class BodyThread(threading.Thread):
                     connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style())
                 mp_drawing.draw_landmarks(
                     image,
+                    results.face_landmarks,
+                    mp_holistic.FACEMESH_TESSELATION,
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style()())
+                mp_drawing.draw_landmarks(
+                    image,
                     results.pose_landmarks,
                     mp_holistic.POSE_CONNECTIONS,
                     landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
