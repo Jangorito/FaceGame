ca9450c0ec5642a8d1fec08868960b7c58145491
psyrb8 <psyrb8@nottingham.ac.uk>
2024-01-09 13:54:34 +0000

    Updated: Basic (but Broken) character movement in Unity

--- Unity/Assets/Scripts/PipeServer.cs
+++ Unity/Assets/Scripts/PipeServer.cs
@@ -9,16 +9,17 @@ using System.Text;
 
 public class PipeServer : MonoBehaviour
 {
-    Body body;
-    public Transform model;
 
+    private const int LANDMARK_COUNT = 543;
     private NamedPipeServerStream server;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        body = new Body(model);
+    private Vector3[] instances = new Vector3[LANDMARK_COUNT];
+    private Vector3[] localPosition = new Vector3[LANDMARK_COUNT];
+    private float maxSpeed = 50f;
 
+
+    // Start is called before the first frame update
+    void Start() {
         Thread startServerThread = new Thread(runServer);
         Debug.Log("Starting Pipe Server");
         startServerThread.Start();
@@ -26,13 +27,12 @@ public class PipeServer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        body.update();
+    void Update() {
+        updateInstances();
+
     }
 
     private void runServer() {
-
         /* Open the named Pipe */
         server = new NamedPipeServerStream("UnityMediaPipeBody");
 
@@ -48,12 +48,11 @@ public class PipeServer : MonoBehaviour
                 var str = new string(br.ReadChars(len));
 
                 string[] lines = str.Split('\n');
-                int lenLine = 0;
-                foreach (string l in lines)
+                foreach (string line in lines)
                 {
-                    if (string.IsNullOrWhiteSpace(l))
+                    if (string.IsNullOrWhiteSpace(line))
                         continue;
-                    body.updateLandmark(lenLine++, l);
+                    parseInput(line);
                 }
 
             } catch (EndOfStreamException)
@@ -67,4 +66,31 @@ public class PipeServer : MonoBehaviour
         server.Close();
         server.Dispose();
     }
+
+    /* Converts the String line to its respective Data */
+    private void parseInput(string line) {
+        string[] parts = line.Split('|');
+        /* Index: 0, x: 1, y: 2, z: 3 */
+        if (parts.Length != 4) {
+            Debug.Log("Invalid Input Detected - " + line);
+            return;
+        }
+
+        int index = int.Parse(parts[0]);
+        Vector3 position = new Vector3(float.Parse(parts[1]), float.Parse(parts[2]), float.Parse(parts[3]));
+        localPosition[index] = position;        
+    }
+
+    /* Uses the localPosition array to move the instances */
+    private void updateInstances() {
+        for (int i = 0; i < LANDMARK_COUNT; i++) {
+            instances[i] = Vector3.MoveTowards(instances[i], localPosition[i],
+                Time.deltaTime * maxSpeed);
+        }
+    }
+
+    public Vector3 getLandmark(Landmark mark) {
+        return instances[(int)mark];
+    }
+
 }
