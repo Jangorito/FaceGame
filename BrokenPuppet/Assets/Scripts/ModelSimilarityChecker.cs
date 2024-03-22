c570b67416cc8f41bd7186bc234636139e1519f0
Jack Brannan <psyjb25@exmail.nottingham.ac.uk>
2024-03-22 17:46:25 +0000

    Reworked shadow avatar to be in the center between the 2 puppets

--- BrokenPuppet/Assets/Scripts/ModelSimilarityChecker.cs
+++ BrokenPuppet/Assets/Scripts/ModelSimilarityChecker.cs
@@ -36,60 +36,39 @@ public class ModelSimilarityChecker : MonoBehaviour
     {
         Timer initialTimer = new Timer();
         initialTimer.Interval = 8000;
-        initialTimer.Elapsed += InitialEvent;
+        initialTimer.AutoReset = false; // Set AutoReset to false to ensure it only triggers once
+        initialTimer.Elapsed += (sender, args) =>
+        {
+            // Call CheckModels after 8 seconds
+            //CheckModels();
+
+            // Dispose of the timer after it's used
+            initialTimer.Stop();
+            initialTimer.Dispose();
+        };
+
         Debug.Log("Initial 8 Seconds started");
         initialTimer.Start();
     }
-    private void InitialEvent(object source, ElapsedEventArgs e)
-    {
-        Timer timer = new Timer();
-        timer.Interval = 2000; // 2 seconds
-        timer.Elapsed += OnTimedEvent;
-        timer.Enabled = true;
-        timer.Start();
 
-        Debug.Log("Timer started");
-        EditorApplication.playModeStateChanged += OnPlayModeStateChange;
-        ((Timer)source).Stop();
-        ((Timer)source).Dispose();
-        Debug.Log("Initial Timer Finished");
-        return;
-    }
-
-    static public void OnPlayModeStateChange(PlayModeStateChange change)
+    private void Update()
     {
-        state = change;
-        Debug.Log(state);
-    }
-
-    private void OnTimedEvent(object source, ElapsedEventArgs e)
-    {
-        //Debug.Log(state);
-        if (state == PlayModeStateChange.ExitingPlayMode || state == PlayModeStateChange.EnteredEditMode)
+        if (!Successful) // Only check models if the round is not successful
         {
-            ((Timer)source).Stop();
-            ((Timer)source).Dispose();
-            //Debug.Log("Game ended");
-        }
-        if (Successful)
-        {
-            // Stop the timer
-            ((Timer)source).Stop();
-            ((Timer)source).Dispose(); // Dispose the timer to release resources
-            //Debug.Log("Timer stopped.");
-            return;
-        }
-        //Debug.Log("Checking Models");
+            Vector3 puppetPosition = getPuppetAvatar().transform.position;
+            Vector3 ghostPosition = getShadowAvatar().transform.position;
+            Debug.Log("********" + puppetPosition + " " + ghostPosition);
 
-        // Perform actions every 2 seconds
-        BrokenPuppet = getPuppetAvatar();
-        GhostAvatar = getShadowAvatar();
-        //Gets the puppets bones
-        BrokenPuppetBones = BrokenPuppet.GetComponentInChildren<SkinnedMeshRenderer>().bones;
-        //Gets the shadows bones
-        ShadowCharacterBones = GhostAvatar.GetComponentInChildren<SkinnedMeshRenderer>().bones;
-        GetVectors();
-        Successful = IsModelNear(PuppetVectors, GhostVectors);  
+            // Perform actions every frame
+            BrokenPuppet = getPuppetAvatar();
+            GhostAvatar = getShadowAvatar();
+            //Gets the puppets bones
+            BrokenPuppetBones = BrokenPuppet.GetComponentInChildren<SkinnedMeshRenderer>().bones;
+            //Gets the shadows bones
+            ShadowCharacterBones = GhostAvatar.GetComponentInChildren<SkinnedMeshRenderer>().bones;
+            GetVectors();
+            Successful = IsModelNear(PuppetVectors, GhostVectors, puppetPosition, ghostPosition);
+        }
     }
 
     //Functions to gathers vectors into an array
@@ -112,11 +91,6 @@ public class ModelSimilarityChecker : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    private void Update()
-    {       
-    }
-
     //Gets the shadow "ghost" avatar the user has to match
     private ShadowAvatar getShadowAvatar()
     {
@@ -135,14 +109,18 @@ public class ModelSimilarityChecker : MonoBehaviour
         return avatar;
     }
 
-    public bool IsModelNear(Vector3[] Puppet, Vector3[] Ghost)
+    public bool IsModelNear(Vector3[] Puppet, Vector3[] Ghost, Vector3 puppetPosition, Vector3 ghostPosition)
     {
         for (int i = 0; i < Puppet.Length; i++)
         {
-            //Takes the distance between the puppet and ghost in terms of vectors
-            float distance = Vector3.Distance(Puppet[i], Ghost[i]);
-            //print($"Puppet vector: {Puppet[i]} Ghost vector: {Ghost[i]} Distance: {distance}");
-            //checks if every bone is <0.1 units away from the corresponding ghost one
+            // Adjust the positions by adding the offsets
+            Vector3 adjustedPuppetPosition = Puppet[i] + puppetPosition;
+            Vector3 adjustedGhostPosition = Ghost[i] + ghostPosition;
+
+            // Takes the distance between the puppet and ghost in terms of vectors
+            float distance = Vector3.Distance(adjustedPuppetPosition, adjustedGhostPosition);
+
+            // Checks if every bone is <0.1 units away from the corresponding ghost one
             if (distance > 0.1)
                 return false;
         }
