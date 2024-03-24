2c309bca3de2b628e158d3bfd01d6da018f20002
Oliver Williams <psyow3@nottingham.ac.uk>
2024-03-24 09:44:35 +0000

    Refactor: Player1 body completly refactored

--- mediapipe/body.py
+++ mediapipe/body.py
@@ -7,7 +7,8 @@ import global_vars  #  global_vars module is defined in global_vars.py
 from pythonosc.udp_client import SimpleUDPClient #to set up and use udp clients for sending OSC data
 
 
-custom_style =""
+
+#SUBSETTING OF LANDMARKS
 
 POSE_CONNECTIONS = frozenset([(0, 1), (1, 2), (2, 3), (3, 7), (0, 4), (4, 5),
                               (5, 6), (6, 8), (9, 10), (11, 12), (11, 13),
@@ -76,62 +77,52 @@ FACEMESH_FACE_OVAL = frozenset([(10, 297), (297, 284), (284, 389), (389, 454),
                                 (172, 132), (132, 234),  (234, 162), (162, 54), 
                                 (54, 67), (67, 10)])
 #face oval goes round clockwise from the top
-
 # 10 FACE_OVAL_1
-
 # 297 FACE_OVAL_2
-
 # 284 FACE_OVAL_3
-
 # 389 FACE_OVAL_4
-
 # 454 FACE_OVAL_5
-
 # 361 FACE_OVAL_6
-
 # 397 FACE_OVAL_7
-
 # 379 FACE_OVAL_8
-
 # 400 FACE_OVAL_9
-
 # 152 FACE_OVAL_10
-
 # 176 FACE_OVAL_11
-
 # 150 FACE_OVAL_12
-
 # 172 FACE_OVAL_13
-
 # 132 FACE_OVAL_14
-
 # 234 FACE_OVAL_15
-
 # 162 FACE_OVAL_16
-
 # 54 FACE_OVAL_17
-
 # 67 FACE_OVAL_18
 
+
+#FUNCTIONS FOR SUBSETTING OF LANDMARKS
+#function for taking the union of all the selected landmarks
+#to remove repeated landmarks
 FACE_CONNECTIONS = frozenset().union(*[
     FACEMESH_LIPS, FACEMESH_LEFT_EYE, FACEMESH_LEFT_EYEBROW, FACEMESH_RIGHT_EYE,
     FACEMESH_RIGHT_EYEBROW, FACEMESH_FACE_OVAL
+    # 17 + 8 + 4 + 8 + 4 +16 = 57 landmarks for the face
 ])
-# 17 + 8 + 4 + 8 + 4 +16 = 57 landmarks for the face
 
+
+#GLOBALVARS
+custom_style =""
 custom_connections = list(POSE_CONNECTIONS)
 custom_face_connections = list(FACE_CONNECTIONS)
-
 custom_face_landmarks = []
+custom_face_landmarks = list(set(custom_face_landmarks))
 
+#function for creating the landmarks list
 for tuple in custom_face_connections:
     int1, int2 = tuple
     custom_face_landmarks.extend([int1, int2])
 
-custom_face_landmarks = list(set(custom_face_landmarks))
 
-# Define a thread for capturing video frames
+#Thread for capturing video frames
 class CaptureThread(threading.Thread):
+    #camera data 
     cap = None
     ret = None
     frame = None
@@ -174,34 +165,27 @@ class CaptureThread(threading.Thread):
             self.cap.set(cv2.CAP_PROP_FPS, global_vars.FPS)
             self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, global_vars.WIDTH)
             self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, global_vars.HEIGHT)
-
-
-
-
-
-# Define a thread for the processing of landmarks
+#Thread for the processing of landmarks
 class BodyThread(threading.Thread):
     data = ""
     client = None
     timeSinceCheckedConnection = 0
+    #Function that handeles run procedure (main function equivelant)
     def run(self):
         # Import necessary modules from the Mediapipe library
         mp_drawing = mp.solutions.drawing_utils
         mp_drawing_styles = mp.solutions.drawing_styles
         mp_holistic = mp.solutions.holistic
         self.CustomLandmarksAndConnections(mp_drawing_styles)
-
         # Start the CaptureThread to capture video frames
         capture = CaptureThread()
         capture.start()
-
         # Create a Mediapipe Holistic instance for processing body landmarks
         with mp_holistic.Holistic(min_detection_confidence=0.8, min_tracking_confidence=0.5, refine_face_landmarks = False) as holistic:
             # Wait until the camera is running before starting body landmark processing
             self.WaitForCamera(capture)
             if global_vars.DEBUG:
                 print("Beginning capture")
-
             # Process body landmarks while the camera is open
             while not global_vars.KILL_THREADS and capture.cap.isOpened():
                 image = capture.frame
@@ -214,7 +198,6 @@ class BodyThread(threading.Thread):
                 image.flags.writeable = True
                 # Draw landmarks on the image
                 self.DrawFaceLandmarks(mp_drawing, mp_drawing_styles, mp_holistic, image, results)
-                #self.DrawFaceTesselation(mp_drawing, mp_drawing_styles, mp_holistic, image, results)
                 self.DrawPoseLandmarks(mp_drawing, mp_drawing_styles, image, results)
                 self.DrawCustomPoseLandmarks(mp_drawing, mp_drawing_styles, mp_holistic, image, results)
                 self.DrawLeftHandLandmarksAndConnections(mp_drawing, mp_drawing_styles, mp_holistic, image, results)
@@ -227,16 +210,13 @@ class BodyThread(threading.Thread):
                 # Break the loop if the 'Esc' key is pressed
                 if cv2.waitKey(5) & 0xFF == 27:
                     break
-
                 # Debugging and communication with Unity project
                 if global_vars.DEBUG:
                     print(time.time() - self.timeSinceCheckedConnection)
-
                 if self.client is None and time.time() - self.timeSinceCheckedConnection >= 1 and not global_vars.PIPE_LINE_DEBUG:
                     ip = "127.0.0.1"
                     port = 5005
                     self.client = SimpleUDPClient(ip, port)  # Create client
-
                 if self.client is not None or global_vars.PIPE_LINE_DEBUG:
                     # Set up data for OSC messaging
                     self.data = ""
@@ -245,32 +225,25 @@ class BodyThread(threading.Thread):
                     self.CollateLeftHandLandmarks(results)
                     self.CollateRightHandLandmarks(results)
                     if self.data:
-                        #s = self.data.encode('utf-8')
                         s = self.data.encode('utf-8')
                         self.client.send_message("/PythonData", s)   # Send OSC message
-                        #print(s)
                     if global_vars.DEBUG:
                         print("Data is empty. Skipping sending OSC message.")
-                    # s = self.data.encode('utf-8')
-                    # self.client.send_message("/PythonData", s)   # Send OSC message
-
             # Close the named pipe and destroy OpenCV windows
         #self.pipe.close()
         # Release the video capture when done
         capture.cap.release()
         cv2.destroyAllWindows()
-
+    #Function for removing the hand landmarks not in the subset
     def RemoveHandLandmarks(self, mp_holistic, results):
         if results.pose_landmarks:
-            #results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_WRIST].visibility = 0.0
             results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_PINKY].visibility = 0.0
             results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_THUMB].visibility = 0.0
             results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_INDEX].visibility = 0.0
-            #results.pose_landmarks.landmark[mp_holistic.PoseLandmark.RIGHT_WRIST].visibility = 0.0
             results.pose_landmarks.landmark[mp_holistic.PoseLandmark.RIGHT_PINKY].visibility = 0.0
             results.pose_landmarks.landmark[mp_holistic.PoseLandmark.RIGHT_THUMB].visibility = 0.0
             results.pose_landmarks.landmark[mp_holistic.PoseLandmark.RIGHT_INDEX].visibility = 0.0
-
+    #Function for removing face hand landmarks not in the subset
     def RemovePoseFaceLandmarks(self, mp_holistic, results):
         if results.pose_landmarks:
             results.pose_landmarks.landmark[mp_holistic.PoseLandmark.NOSE].visibility = 0.0
@@ -284,44 +257,20 @@ class BodyThread(threading.Thread):
             results.pose_landmarks.landmark[mp_holistic.PoseLandmark.RIGHT_EAR].visibility = 0.0
             results.pose_landmarks.landmark[mp_holistic.PoseLandmark.MOUTH_LEFT].visibility = 0.0
             results.pose_landmarks.landmark[mp_holistic.PoseLandmark.MOUTH_RIGHT].visibility = 0.0
-
-            # results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_WRIST].ClearField("x")
-            # results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_WRIST].ClearField("y")
-            # results.pose_landmarks.landmark[mp_holistic.PoseLandmark.LEFT_WRIST].ClearField("z")
-            
+    #Function for setting the custom landmarks to be displayed in webcam raw image data
     def CustomLandmarksAndConnections(self, mp_drawing_styles):
         hand_landmarks = mp_drawing_styles.get_default_hand_landmarks_style()
         custom_style = mp_drawing_styles.get_default_pose_landmarks_style()
-
         # Get the hand landmark indices to be removed
         hand_landmark_indices = [landmark.value for landmark in hand_landmarks]
-
         # Remove hand landmarks from custom style
         custom_style = {landmark: style for landmark, style in custom_style.items() if landmark not in hand_landmark_indices}
-
+    #Function for waiting for the camera to initialise
     def WaitForCamera(self, capture):
         while not global_vars.KILL_THREADS and capture.isRunning == False:
             print("Waiting for camera and capture thread.")
             time.sleep(0.5)
-
-    # def SendDataOverPipe(self, s):
-    #     try:
-    #         self.pipe.write(struct.pack('I', len(s)) + s)
-    #         self.pipe.seek(0)
-    #     except Exception as ex:
-    #         print("Failed to write to pipe. Is the unity project open?")
-    #         self.pipe = None
-
-    # def OpenNamedPipe(self):
-    #     try:
-    #                     # Attempt to open a named pipe for communication with Unity
-    #         self.pipe = open(r'\\.\pipe\UnityMediaPipeBody', 'r+b', 0)
-    #         print("entered")
-    #     except FileNotFoundError:
-    #         print("Waiting for Unity project to run...")
-    #         self.pipe = None
-    #     self.timeSinceCheckedConnection = time.time()
-
+    #Function for collecting the right hand landmarks and adding them to data to be sent 
     def CollateRightHandLandmarks(self, results):
         if results.right_hand_landmarks:
             right_hand_landmarks = results.right_hand_landmarks
@@ -330,7 +279,7 @@ class BodyThread(threading.Thread):
                                 "RH", i, right_hand_landmarks.landmark[i].x,
                                 right_hand_landmarks.landmark[i].y,
                                 right_hand_landmarks.landmark[i].z))
-                                    #RH
+    #Function for collecting the left hand landmarks and adding them to data to be sent 
     def CollateLeftHandLandmarks(self, results):
         if results.left_hand_landmarks:
             left_hand_landmarks = results.left_hand_landmarks
@@ -339,7 +288,7 @@ class BodyThread(threading.Thread):
                                 "LH", i, left_hand_landmarks.landmark[i].x,
                                 left_hand_landmarks.landmark[i].y,
                                 left_hand_landmarks.landmark[i].z))
-                                    # LH = Left Hand
+    #Function for collecting the facial landmarks and adding them to data to be sent 
     def CollateFaceLandmarks(self, results):
         if  results.face_landmarks:
             face_landmarks = results.face_landmarks
@@ -348,6 +297,7 @@ class BodyThread(threading.Thread):
                     continue
                  self.data +=("{}|{}|{}|{}|{}\n".format("FL", i, face_landmarks.landmark[i].x, face_landmarks.landmark[i].y, face_landmarks.landmark[i].z))
                                     # FL = Face Landmarks
+    #Function for collecting the pose (body) landmarks and adding them to data to be sent 
     def CollatePoseLandmarks(self, results):
         if results.pose_world_landmarks:
             hand_world_landmarks = results.pose_world_landmarks
@@ -357,10 +307,7 @@ class BodyThread(threading.Thread):
                self.data +=("{}|{}|{}|{}|{}\n".format("PL", i, hand_world_landmarks.landmark[i].x, # PL = Pose Landmarks
                                                                     hand_world_landmarks.landmark[i].y,
                                                                     hand_world_landmarks.landmark[i].z))
-        
-        
-
-
+    #Function for drawing the right hand on the mediapipe image 
     def DrawRightHandLandmarksAndConnections(self, mp_drawing, mp_drawing_styles, mp_holistic, image, results):
         mp_drawing.draw_landmarks(
                     image,
@@ -368,7 +315,7 @@ class BodyThread(threading.Thread):
                     mp_holistic.HAND_CONNECTIONS,
                     landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
                     connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style())
-
+    #Function for drawing the left hand on the mediapipe image 
     def DrawLeftHandLandmarksAndConnections(self, mp_drawing, mp_drawing_styles, mp_holistic, image, results):
         mp_drawing.draw_landmarks(
                     image,
@@ -376,39 +323,25 @@ class BodyThread(threading.Thread):
                     mp_holistic.HAND_CONNECTIONS,
                     landmark_drawing_spec=mp_drawing_styles.get_default_hand_landmarks_style(),
                     connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style())
-
+    #Function for drawing the pose (body) on the mediapipe image
     def DrawPoseLandmarks(self, mp_drawing, mp_drawing_styles, image, results):
         mp_drawing.draw_landmarks(
                     image,
                     results.pose_landmarks,
-                    #mp_holistic.POSE_CONNECTIONS,
-                    #landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
                     connections = custom_connections,
-                    #landmark_drawing_spec=custom_style)
-                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-        
-    def DrawCustomPoseLandmarks(self, mp_drawing, mp_drawing_styles, mp_holistic, image, results): #doesnt work atm
+                    landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())  
+    #Function for drawing the custom pose (body) on the mediapipe image
+    def DrawCustomPoseLandmarks(self, mp_drawing, mp_drawing_styles, mp_holistic, image, results):
         custom_connections2 = [
-        # Custom connection from left elbow to left wrist
         (mp_holistic.PoseLandmark.LEFT_ELBOW, mp_holistic.HandLandmark.WRIST),
-        # Custom connection from right elbow to right wrist
         (mp_holistic.PoseLandmark.RIGHT_ELBOW, mp_holistic.HandLandmark.WRIST)
         ]
         mp_drawing.draw_landmarks(
                         image,
                         results.pose_landmarks,
                         connections = custom_connections2,
-                        #landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style(),
                         connection_drawing_spec=mp_drawing_styles.get_default_hand_connections_style())
-
-    def DrawFaceTesselation(self, mp_drawing, mp_drawing_styles, mp_holistic, image, results):
-        mp_drawing.draw_landmarks(
-                    image,
-                    results.face_landmarks,
-                    mp_holistic.FACEMESH_TESSELATION,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_tesselation_style())
-
+    #Function for drawing the custom face on the mediapipe image
     def DrawFaceLandmarks(self, mp_drawing, mp_drawing_styles, mp_holistic, image, results):
         drawing_spec = mp_drawing.DrawingSpec(color = (0, 0, 255), thickness = 1, circle_radius = 1)
 
@@ -427,25 +360,18 @@ class BodyThread(threading.Thread):
                     results.face_landmarks,
                     connections = custom_face_connections,
                     landmark_drawing_spec=drawing_spec)
-    
-
-
-
+    #Function for sending the OSC data for the raw Image webcam data
     def send(self, image):
         # Set up OSC client
         client = SimpleUDPClient("127.0.0.1", 5008)  # OSC server address and port
-
         # Convert the image to bytes
         retval, buffer = cv2.imencode('.jpg', image)
-
         # Check if image encoding was successful
         if not retval:
             print("Error: Failed to encode the image.")
             return
-
         # Convert the image buffer to bytes
         data = buffer.tobytes()
-
         try:
             # Send the image bytes via OSC
             client.send_message("/video", data)
